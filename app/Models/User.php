717b0a45ba66d5800d1735632048522a2f452a54
“[mbaye98]” <mbayediop2309481@gmail.com>
2023-10-05 14:34:46 +0200

    commitation  apres avoir gerer la milticites entres les differentes tables de ma base

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,11 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+
+    public function commandes()
+    {
+        return $this->hasMany(Commande::class);
+    }
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
